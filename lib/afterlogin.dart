14bfb324b9ac27998f83166bb16bfcae9b013808
tm410 <tmjg410@gmail.com>
2024-08-23 01:51:09 +0900

    firestore added with bug

---
+++ lib/afterlogin.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/main.dart';
+import 'package:flutter_application_1/post.dart';
+
+class AfterLoginPage extends StatelessWidget {
+  const AfterLoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("After login"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () async {
+              // ログイン画面に遷移＋チャット画面を破棄
+              await Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) {
+                  return SignInPage();
+                }),
+              );
+            },
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          // 投稿画面に遷移
+          await Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              return PostPage(??);
+            }),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:flutter_application_1/afterlogin.dart';
 import 'package:flutter_application_1/firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main() async {
   // main 関数でも async が使えます
@@ -35,7 +37,8 @@ class SignInPage extends StatefulWidget {
 class _SignInPageState extends State<SignInPage> {
   Future<void> signInWithGoogle() async {
     // GoogleSignIn をして得られた情報を Firebase と関連づけることをやっています。
-    final googleUser = await GoogleSignIn(scopes: ['profile', 'email']).signIn();
+    final googleUser =
+        await GoogleSignIn(scopes: ['profile', 'email']).signIn();
 
     final googleAuth = await googleUser?.authentication;
 
@@ -55,11 +58,16 @@ class _SignInPageState extends State<SignInPage> {
       ),
       body: Center(
         child: ElevatedButton(
+          //この中はボタンについての設定
           child: const Text('GoogleSignIn'),
           onPressed: () async {
             await signInWithGoogle();
             // ログインが成功すると FirebaseAuth.instance.currentUser にログイン中のユーザーの情報が入ります
             print(FirebaseAuth.instance.currentUser?.displayName);
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AfterLoginPage()),
+            );
           },
         ),
       ),
